f192ad090821dc97ee7aad3503d9f2d2e9a719ea
Domags99 <Djaguilar@mail.csuchico.edu>
2023-05-09 11:10:21 -0700

    car jump script
    car jump

---
+++ projectExplo/Assets/Scripts/CarJump.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarJump : MonoBehaviour
+{
+    public float jumpforce = 10f;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpforce, (ForceMode)ForceMode2D.Impulse);
+
+        }
+
+    }
+}
